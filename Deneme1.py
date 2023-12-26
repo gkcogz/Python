a9d41abab3f5f17e1413e4de28d028364c6dc69a
gkcogz <150557473+gkcogz@users.noreply.github.com>
2023-12-26 18:57:47 +0100

    Update Deneme1.py

--- Deneme1.py
+++ Deneme1.py
@@ -1,18 +1,1 @@
 
-# Online Python - IDE, Editor, Compiler, Interpreter
-# Deneme
-def quad1(a, b, c):
-    determ=(b*b-4*a*c)**0.5
-    x=(-b+determ)/(2*a)
-    return x
-
-def quad2(a, b, c):
-    determ=(b*b-4*a*c)**0.5
-    x=(-b-determ)/(2*a)
-    return x
-    
-def print_x(a, b, c):
-    print("The x1 value is",quad1(a, b, c))
-    print(",and the x2 value is",quad2(a, b, c))
-    
-print_x(1,-1,-1)
